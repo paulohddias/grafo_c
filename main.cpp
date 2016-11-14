99cc0abe59acac648d40dcfca3004eb0972e073e
paulohddias <paulohddias@gmail.com>
2016-11-14 16:49:43 -0200

    subindo projeto

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: PauloDias
+ *
+ * Created on 14 de Novembro de 2016, 16:10
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    std::cout<<"hello"<<endl;
+    return 0;
+}
+
